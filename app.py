bd807366fd37ffce48a374c1bf0612f1867da44e
Vaishnavi-cyber-blip <72164140+Vaishnavi-cyber-blip@users.noreply.github.com>
2024-07-26 12:52:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def get_img_as_base64(file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-img = get_img_as_base64("wall6.png")
+img = get_img_as_base64("assets/wall6.png")
 
 page_bg_img = f"""
 <style>
